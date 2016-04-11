813f43244476ab8c24cb13f19c50881ad43c1aa9
David Knoll <david@futurefirst.org.uk>
2016-04-11 14:44:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# SMS STOP/unsubscribe processor
+An extension for CiviCRM.
+Acts upon received SMS messages beginning with keywords such as STOP,
+which indicate the remote contact's wish to unsubscribe. This extension
+replies to the contact acknowledging their request, and sets them as
+'do not SMS'. It is an attempt at following section 10.1.1
+(STOP command implementation) of the Clickatell two-way technical guide.
+
+### Installation
+Install in the usual ways, either from the CiviCRM extensions repository or
+by downloading directly from GitHub. If you download a snapshot you may need
+to rename the extension's directory to the extension's key
+(`uk.org.futurefirst.networks.smsstop`).
+
+This extension *requires* CiviCooP's SMS API extension (`org.civicoop.smsapi`)
+in order to send the acknowledgement SMS.
+
+### Features
+* Acknowledge unsubscribe requests with a custom reply- you can, for example,
+write an apology that includes a number to call for further assistance
+* Automatically set the contact to 'do not SMS', so CiviCRM knows not to
+attempt to send to them in future
+* The above should co-operate with the SMS Survey extension, stopping the next
+question from being sent to that contact
+
+### Links
+* <http://www.futurefirst.org.uk/>
+* <https://github.com/futurefirst/uk.org.futurefirst.networks.smsstop/wiki>
+* <https://github.com/CiviCooP/org.civicoop.smsapi>
+* <https://github.com/futurefirst/org.thirdsectordesign.chainsms>
+
+### Licensing
+Copyright (C)2016 [Future First](http://www.futurefirst.org.uk/).
+Future First Alumni Limited (formerly known as The Camden Future First Network
+Limited), operating under the public name 'Future First', is a registered
+charity in England and Wales (1135638), and in Scotland (SC043973).
+
+Maintained by [David Knoll](mailto:david@futurefirst.org.uk).
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ smsstop.php
@@ -0,0 +1,204 @@
+<?php
+
+require_once 'smsstop.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function smsstop_civicrm_config(&$config) {
+  _smsstop_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function smsstop_civicrm_xmlMenu(&$files) {
+  _smsstop_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function smsstop_civicrm_install() {
+  _smsstop_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function smsstop_civicrm_uninstall() {
+  _smsstop_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function smsstop_civicrm_enable() {
+  _smsstop_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function smsstop_civicrm_disable() {
+  _smsstop_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed
+ *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function smsstop_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _smsstop_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function smsstop_civicrm_managed(&$entities) {
+  _smsstop_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function smsstop_civicrm_caseTypes(&$caseTypes) {
+  _smsstop_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function smsstop_civicrm_angularModules(&$angularModules) {
+_smsstop_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function smsstop_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _smsstop_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Functions below this ship commented out. Uncomment as required.
+ *
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function smsstop_civicrm_preProcess($formName, &$form) {
+
+}
+
+*/
+
+/**
+ * Implements hook_civicrm_pre().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_pre
+ */
+function smsstop_civicrm_pre($op, $objectName, $id, &$params) {
+  // Must be an Inbound SMS activity, being created, beginning with a keyword
+  if ($op != 'create' || $objectName != 'Activity') {
+    return;
+  }
+
+  $body = strtoupper(trim($params['details']));
+  if (!preg_match("/^(STOP|END|CANCEL|UNSUBSCRIBE|QUIT)\b/", $body)) {
+    return;
+  }
+
+  $activityTypeID = CRM_Core_OptionGroup::getValue('activity_type', 'Inbound SMS', 'name');
+  if ($params['activity_type_id'] != $activityTypeID) {
+    return;
+  }
+
+  // Get contact IDs
+  // Note: Personally I think CRM_SMS_Provider::processInbound does these the
+  // wrong way round, as it doesn't match inbound emails.
+  $remoteCid = $params['target_contact_id'];
+  $ourCid    = $params['source_contact_id'] ?: $params['target_contact_id'];
+
+  CRM_Core_Error::debug_log_message("SMS STOP message received from cid $remoteCid");
+
+  // Get default SMS provider and our template
+  $providerId = CRM_Core_DAO::singleValueQuery("
+    SELECT `id`
+      FROM `civicrm_sms_provider`
+     WHERE `is_default` IS TRUE
+       AND `is_active`  IS TRUE
+  ORDER BY `id`         ASC
+     LIMIT 1
+  ");
+  $templateId = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_MessageTemplate', 'SMS STOP acknowledgement', 'id', 'title');
+
+  // Send an acknowledgement
+  // Note: from_contact_id is required if we're not logged in
+  // (which is the case during an SMS callback), as the Outbound SMS
+  // being created needs a source contact.
+  $ack_result = civicrm_api('Sms', 'Send', array(
+    'version'         => 3,
+    'contact_id'      => $remoteCid,
+    'from_contact_id' => $ourCid,
+    'template_id'     => $templateId,
+    'provider_id'     => $providerId,
+  ));
+  if (civicrm_error($ack_result)) {
+    CRM_Core_Error::debug_log_message("Error sending SMS STOP acknowledgement to cid $remoteCid: " . $ack_result['error_message']);
+  }
+
+  // Set them as 'do not SMS'
+  $set_result = civicrm_api('Contact', 'create', array(
+    'version'    => 3,
+    'id'         => $remoteCid,
+    'do_not_sms' => 1,
+  ));
+  if (civicrm_error($set_result)) {
+    CRM_Core_Error::debug_log_message("Error setting 'do not SMS' on cid $remoteCid: " . $set_result['error_message']);
+  }
+}
